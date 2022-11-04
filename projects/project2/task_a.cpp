895cbbef88ea03b38d3d30d06cfd94c3d4f2b167
Michael Shen <michaelshen85@gmail.com>
2022-11-03 23:51:55 -0400

    Add task_a.cpp

--- projects/project2/task_a.cpp
+++ projects/project2/task_a.cpp
@@ -4,3 +4,55 @@ Instructor: Genadiy Maryash
 Course: CSCI 135 
 Project: 2A
 */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    
+    ifstream input;
+    input.open("input.txt");
+    ofstream output;
+
+    string line, file_name, width;
+    getline(input, line);
+    int max_width, i = 0;
+
+    while (line[i] != ';') width += line[i++];
+    max_width = stoi(width);
+    i++;
+    while (line[i] != ';') file_name += line[i++];
+    output.open(file_name);
+
+    int line_width = max_width;
+    string word;
+
+    while (getline(input, line)) {
+        if (line.length() == 0) {
+            output << "\n\n";
+        } else if (line.length() <= max_width) {
+            output << line << '\n';
+        } else {
+            i = 0;
+            word = "";
+            while (i <= line.length()) {
+                if (line_width - word.length() <= 0) {
+                    line_width = max_width;
+                    output << '\n';
+                }
+                if (isspace(line[i]) || i == line.length()) {
+                    output << word << " ";
+                    line_width -= word.length() + 1;
+                    word = "";
+                } else {
+                    word += line[i];
+                }
+                i++;
+            }
+        }
+    }
+    return 0;
+}
