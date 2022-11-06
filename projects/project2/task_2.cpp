cbb77ee2220e00a56926b9265423a758e3d21a30
Michael Shen <michaelshen85@gmail.com>
2022-11-06 15:25:02 -0500

    Finish cases for empty and good lines

--- projects/project2/task_2.cpp
+++ projects/project2/task_2.cpp
@@ -30,6 +30,17 @@ vector<string> split(string str, char delimiter=' ') {
     return words;
 }
 
+// tests if all characters in a string are capitalized 
+bool all_caps(string str) {
+    int ascii_val;
+    for (int i = 0; i < str.length(); i++) {
+        ascii_val = (int) str[i];
+        if (ascii_val < 65 || ascii_val > 90 && isalpha(ascii_val)) {
+            return false;
+        }
+    } // FIX ALL_CAPS FUNCTION!!!!!!
+    return true;
+}
 
 int main() {
     
@@ -40,24 +51,49 @@ int main() {
     string line, body_just = "left", head_just = "left";
     getline(input, line);
 
-    vector<string> data = split(line, ';');
-    string file_name = data[data.size()-1];
-    file_name.pop_back();
-    int max_width = stoi(data[0]);
+    vector<string> text = split(line, ';');
+    string file_name = text[text.size()-1];
+    file_name.pop_back(); // removes final semicolon from file_name 
+    int max_width = stoi(text[0]);
     output.open(file_name);
     
-    if (data.size() == 3) {
-        body_just = data[1];
+    if (text.size() == 3) {
+        body_just = text[1];
         head_just = body_just;
     }
 
-    if (data.size() == 4) {
-        body_just = data[1];
-        head_just = data[2];
+    if (text.size() == 4) {
+        body_just = text[1];
+        head_just = text[2];
     }
     
+    string new_line = "", justification = head_just;
     while (getline(input, line)) {
+        cout << all_caps(line) << endl;
+        // case when we encounter the empty line 
+        if (line.length() == 0) {
+            new_line = "";
+            output << "\n\n";
         
+        // case when we encounter a good line
+        } else if (line.length() <= max_width) {
+            all_caps(line) ? justification = head_just : justification = body_just;
+            if (justification == "right") {
+                output << setw(max_width) << line << '\n';
+            } else {
+                output << new_line << '\n';
+            }
+            new_line = "";
+        }
+
+        // case when we encounter a bad line 
+        else {
+            text = split(line);
+            new_line = "";
+            for (auto &w: text) {
+                
+            }
+        }
     }
 
     output.close();
