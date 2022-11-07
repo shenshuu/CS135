aa4716a9e21088838c33338a585e21abd682ced8
Michael Shen <michaelshen85@gmail.com>
2022-11-07 17:37:08 -0500

    Update repo

---
+++ projects/project2/task_1.cpp
@@ -0,0 +1,24 @@
+/*
+Name: Michael Shen 
+Course: CSCI-135
+Instructor: Genadiy Maryash
+Project: 2A
+*/
+
+#include <iostream>
+#include <fstream> 
+#include <string>
+using namespace std;
+
+int main() {
+    int line_width;
+    ofstream fout("output.txt");
+    ifstream fin("input.txt");
+    string line;
+
+    while (getline(fin, line)) {
+
+    }
+
+    return 0;
+}
