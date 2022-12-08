9ea87b276c450fce146d249cdfa54e2fdc3c8d86
Michael Shen <michaelshen85@gmail.com>
2022-12-08 17:47:45 -0500

    Add social3.cpp

--- labs/lab11/social3.cpp
+++ labs/lab11/social3.cpp
@@ -39,6 +39,7 @@ class Network {
         static const int MAX_USERS = 20;
         int numUsers;
         Profile profiles[MAX_USERS];
+        bool following[MAX_USERS][MAX_USERS];
 
         int findID(string usrn) {
             for (int i = 0; i < MAX_USERS; i++) {
@@ -66,20 +67,66 @@ class Network {
                 return true;
             }
 
+            bool follow(string usrn1, string usrn2) {
+                int m = findID(usrn1), n = findID(usrn2);
+                if (m != -1 && n != -1) {
+                    following[m][n] = true;
+                    return true;
+                }
+                return false;
+            }
+
+            void printDot() {
+                cout << "digraph {" << '\n';
+                for (int i = 0; i < MAX_USERS; i++) {
+                    if (profiles[i].getUsername() != "") {
+                        cout << '\t' << "\"@" << profiles[i].getUsername() << "\"" << endl;
+                    }
+                }
+                cout << '\n';
+                for (int i = 0; i < MAX_USERS; i++) {
+                    for (int j = 0; j < MAX_USERS; j++) {
+                        if (profiles[i].getUsername() != "" && profiles[j].getUsername() != "") {
+                            if (following[i][j]) {
+                                cout << '\t' << "\"@" << profiles[i].getUsername() << "\"";
+                                cout << " -> ";
+                                cout << "\"@" << profiles[j].getUsername() << "\"" << endl;
+                            }
+                        }
+                    }
+                }
+                cout << "}";
+            }
+
 };
 
 int main() {
-  Network nw;
-  cout << nw.addUser("mario", "Mario") << endl;     // true (1)
-  cout << nw.addUser("luigi", "Luigi") << endl;     // true (1)
+    Network nw;
+    // add three users
+    nw.addUser("mario", "Mario");
+    nw.addUser("luigi", "Luigi");
+    nw.addUser("yoshi", "Yoshi");
 
-  cout << nw.addUser("mario", "Mario2") << endl;    // false (0)
-  cout << nw.addUser("mario 2", "Mario2") << endl;  // false (0)
-  cout << nw.addUser("mario-2", "Mario2") << endl;  // false (0)
+    // make them follow each other
+    nw.follow("mario", "luigi");
+    nw.follow("mario", "yoshi");
+    nw.follow("luigi", "mario");
+    nw.follow("luigi", "yoshi");
+    nw.follow("yoshi", "mario");
+    nw.follow("yoshi", "luigi");
 
-  for(int i = 2; i < 20; i++)
-      cout << nw.addUser("mario" + to_string(i), 
-                 "Mario" + to_string(i)) << endl;   // true (1)
+    // add a user who does not follow others
+    nw.addUser("wario", "Wario");
+    
+    // add clone users who follow @mario
+    for(int i = 2; i < 6; i++) {
+        string usrn = "mario" + to_string(i);
+        string dspn = "Mario " + to_string(i);
+        nw.addUser(usrn, dspn);
+        nw.follow(usrn, "mario");
+    }
+    // additionally, make @mario2 follow @luigi
+    nw.follow("mario2", "luigi");
 
-  cout << nw.addUser("yoshi", "Yoshi") << endl;     // false (0)
+    nw.printDot();
 }
